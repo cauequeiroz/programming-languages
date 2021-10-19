39b881ff03dca93a4ea3bf78d82483686095afe8
Caue Queiroz <cauenqueiroz@gmail.com>
2021-10-19 17:44:51 -0300

    update week 1 notes

--- part-b/week-1/notes.rkt
+++ part-b/week-1/notes.rkt
@@ -1,2 +1,43 @@
 #lang racket
 (provide (all-defined-out))
+
+; Definitions
+(define name "Caue Queiroz")
+(define x 42)
+(define y (+ x 1))
+
+; Functions
+(define (sum x y)
+  (+ x y))
+
+(define (pow x y)
+  (if (= y 0) 1 (* x (pow x (- y 1)))))
+
+;; Currying
+(define build-multiplication
+  (lambda (x)
+    (lambda (y)
+      (* x y))))
+
+(define square
+  (build-multiplication 2))
+
+(define cube
+  (build-multiplication 3))
+
+; Working with lists
+(define (sum-all xs)
+  (if (null? xs)
+      0
+      (+ (car xs) (sum-all (cdr xs)))))
+
+(define (join-lists l1 l2)
+  (if (null? l1)
+      l2
+      (cons (car l1) (join-lists (cdr l1) l2))))
+
+(define (map-all fn xs)
+  (if (null? xs)
+      null
+      (cons (fn (car xs)) (map-all fn (cdr xs)))))
+
